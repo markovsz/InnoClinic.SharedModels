a6d71b3396e3fd7423d13dc4ffc33749f41847a5
markovsz <58207528+markovsz@users.noreply.github.com>
2023-05-24 15:14:49 +0300

    add photoUrl property to AccountIncomingDto

--- InnoClinic.SharedModels/DTOs/Identity/Incoming/AccountIncomingDto.cs
+++ InnoClinic.SharedModels/DTOs/Identity/Incoming/AccountIncomingDto.cs
@@ -2,6 +2,7 @@
 
 public class AccountIncomingDto
 {
+    public string PhotoUrl { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
     public string ReEnteredPassword { get; set; }
