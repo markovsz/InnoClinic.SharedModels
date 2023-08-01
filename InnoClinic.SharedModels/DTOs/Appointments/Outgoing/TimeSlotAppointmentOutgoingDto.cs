421acb56370db642d3f21dfcd196381961474b45
markovsz <58207528+markovsz@users.noreply.github.com>
2023-08-01 16:06:42 +0300

    add time slots dtos

---
+++ InnoClinic.SharedModels/DTOs/Appointments/Outgoing/TimeSlotAppointmentOutgoingDto.cs
@@ -0,0 +1,7 @@
+﻿namespace InnoClinic.SharedModels.DTOs.Appointments.Outgoing;
+
+public class TimeSlotAppointmentOutgoingDto
+{
+    public DateTime DateTime { get; set; }
+    public Guid ServiceId { get; set; }
+}
---
+++ InnoClinic.SharedModels/DTOs/Appointments/RequestParameters/TimeSlotParameters.cs
@@ -0,0 +1,9 @@
+﻿namespace InnoClinic.SharedModels.DTOs.Appointments.RequestParameters;
+
+public class TimeSlotParameters
+{
+    public int Day { get; set; }
+    public int Month { get; set; }
+    public int Year { get; set; }
+    public Guid DoctorId { get; set; }
+}
